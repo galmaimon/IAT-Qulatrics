e6b67f06887126a36a19c83c7172cfc2ef2424bd
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-06-03 20:07:17 +0300

    Create exampleQIAT.js

---
+++ exampleQIAT.js
@@ -0,0 +1,125 @@
+define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
+
+	function iatExtension(options)
+	{
+		var API = new APIConstructor();		
+		var scorer = new Scorer();
+		var piCurrent = API.getCurrent();
+
+	var posWords = API.shuffle([
+            'Love', 'Cheer', 'Friend', 'Pleasure', 
+            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
+            'Smiling','Cherish', 'Excellent', 'Glad', 
+            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
+            'Excitement', 'Laughing', 'Attractive','Delightful', 
+            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
+            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
+            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph']);
+  var negWords = API.shuffle([
+            'Abuse', 'Grief', 'Poison', 'Sadness', 
+            'Pain', 'Despise', 'Failure', 'Nasty', 
+            'Angry', 'Detest', 'Horrible', 'Negative', 
+            'Ugly', 'Dirty', 'Gross', 'Evil', 
+            'Rotten','Annoy', 'Disaster', 'Horrific',  
+            'Scorn', 'Awful', 'Disgust', 'Hate', 
+            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
+            'Hatred', 'Hurtful', 'Sickening', 'Yucky']);
+
+		//Here we set the settings of our task. 
+		//Read the comments to learn what each parameters means.
+		//You can also do that from the outside, with a dedicated jsp file.
+		var iatObj =
+		{
+			isTouch:false, //Set whether the task is on a touch device.
+			//Set the canvas of the task
+			canvas : {
+				maxWidth: 725,
+				proportions : 0.7,
+				background: '#ffffff',
+				borderWidth: 5,
+				canvasBackground: '#ffffff',
+				borderColor: 'lightblue'
+			},
+			//When scoring, we will consider the compatible condition the pairing condition that requires response with one key to [category1,attribute1] and the other key to [category2,attribute2]
+ category1 : {
+            name : 'Old people', //Will appear in the data.
+            title : {
+                media : {word : 'Old people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'om1.jpg'},
+                {image: 'om2.jpg'},
+                {image: 'om3.jpg'},
+                {image: 'of1.jpg'},
+                {image: 'of2.jpg'},                 
+                {image: 'of3.jpg'}     
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },    
+        category2 :    {
+            name : 'Young people', //Will appear in the data.
+            title : {
+                media : {word : 'Young people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'ym1.jpg'},
+                {image: 'ym2.jpg'},
+                {image: 'ym3.jpg'},
+                {image: 'yf1.jpg'},
+                {image: 'yf2.jpg'},
+                {image: 'yf3.jpg'}
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },
+        attribute1 : {
+            name : 'Bad',
+            title : {
+                media : {word : 'Bad'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: negWords[0]},
+                {word: negWords[1]},
+                {word: negWords[2]},
+                {word: negWords[3]},
+                {word: negWords[4]},
+                {word: negWords[5]},
+                {word: negWords[6]},
+                {word: negWords[7]}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        attribute2 : {
+            name : 'Good',
+            title : {
+                media : {word : 'Good'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: posWords[0]},
+                {word: posWords[1]},
+                {word: posWords[2]},
+                {word: posWords[3]},
+                {word: posWords[4]},
+                {word: posWords[5]},
+                {word: posWords[6]},
+                {word: posWords[7]}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        base_url : {//Where are your images at?
+            image :" https://github.com/galmaimon/IATLab/tree/master/iatage"
+        
+		} 
+	});
+});
