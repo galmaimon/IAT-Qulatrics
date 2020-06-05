37b63138f12a0b1d4f64be0fec37d08e7a6b24d2
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-06-05 12:56:58 +0300

    Update exampleQIATarab.js

--- exampleQIATarab.js
+++ exampleQIATarab.js
@@ -20,33 +20,9 @@ define(['pipAPI', 'https://galmaimon.github.io/iatArab.js'], function(APIConstru
             'Hatred', 'Hurtful', 'Sickening', 'Yucky'])
         });
         var global=API.getGlobal();
-        function tableCreate() {
-            var body = document.getElementsByTagName('body')[0];
-            var tbl = document.createElement('table');
-            tbl.style.width = '100%';
-            tbl.setAttribute('border', '1');
-            var tbdy = document.createElement('tbody');
-            for (var i = 0; i < 3; i++) {
-              var tr = document.createElement('tr');
-              for (var j = 0; j < 2; j++) {
-                if (i == 2 && j == 1) {
-                  break
-                } else {
-                  var td = document.createElement('td');
-                  td.appendChild(document.createTextNode('\u0020'))
-                  i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
-                  tr.appendChild(td)
-                }
-              }
-              tbdy.appendChild(tr);
-            }
-            tbl.appendChild(tbdy);
-            body.appendChild(tbl)
-          }
-          tableCreate();
+        
        
 	return iatExtension({
-		          tableCreate();
 
 		category1 : {
 			name : 'Arab Muslims', //Will appear in the data.
