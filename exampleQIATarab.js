2786943319e21745b1c210c50a753dca5f8df001
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-06-05 12:47:25 +0300

    Update exampleQIATarab.js

--- exampleQIATarab.js
+++ exampleQIATarab.js
@@ -18,8 +18,33 @@ define(['pipAPI', 'https://galmaimon.github.io/iatArab.js'], function(APIConstru
             'Scorn', 'Awful', 'Disgust', 'Hate', 
             'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
             'Hatred', 'Hurtful', 'Sickening', 'Yucky'])
-		});
-
+        });
+        var global=API.getGlobal();
+        function tableCreate() {
+            var body = document.getElementsByTagName('body')[0];
+            var tbl = document.createElement('table');
+            tbl.style.width = '100%';
+            tbl.setAttribute('border', '1');
+            var tbdy = document.createElement('tbody');
+            for (var i = 0; i < 3; i++) {
+              var tr = document.createElement('tr');
+              for (var j = 0; j < 2; j++) {
+                if (i == 2 && j == 1) {
+                  break
+                } else {
+                  var td = document.createElement('td');
+                  td.appendChild(document.createTextNode('\u0020'))
+                  i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+                  tr.appendChild(td)
+                }
+              }
+              tbdy.appendChild(tr);
+            }
+            tbl.appendChild(tbdy);
+            body.appendChild(tbl)
+          }
+          tableCreate();
+       
 	return iatExtension({
 		category1 : {
 			name : 'Arab Muslims', //Will appear in the data.
