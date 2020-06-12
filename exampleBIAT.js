d642c83cb38f009a782bbf5f52422d833dab01e1
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-06-12 08:39:19 +0300

    Create exampleBIAT.js

---
+++ exampleBIAT.js
@@ -0,0 +1,10 @@
+define(['pipAPI', 'https://github.com/baranan/minno-tasks/BIAT/biat6.js'], function(APIConstructor, iatExtension){
+  var API = new APIConstructor();
+
+	return iatExtension({
+		
+});
+
+
+
+
