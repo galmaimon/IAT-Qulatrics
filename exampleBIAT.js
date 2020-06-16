799c74378b3c5a811a5dc1cc283ea82cab410155
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-06-16 13:00:33 +0300

    Update exampleBIAT.js

--- exampleBIAT.js
+++ exampleBIAT.js
@@ -1,4 +1,97 @@
-define(['pipAPI', 'https://github.com/baranan/minno-tasks/BIAT/biat6.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.2.8/BIAT/biat6.js'], function(APIConstructor, iatExtension){
   var API = new APIConstructor();
+
+	
+	return iatExtension({
+		practiceCategory1 : 
+			{
+				name : 'Mammals', //Will appear in the data.
+				title : {
+					media : {word : 'Mammals'}, //Name of the category presented in the task.
+					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
+					height : 4, //Height (because we need to know where to put the next item in the title)
+					startStimulus : { 
+					//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
+						media : {word : 'Dogs, Horses, Cows, Lions'}, 
+						css : {color:'#31b404','font-size':'1em'}, 
+						height : 2
+					}
+				}, 
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word : 'Dogs'}, 
+					{word : 'Horses'}, 
+					{word : 'Lions'}, 
+					{word : 'Cows'}
+				], 
+				//Stimulus css (style of the stimuli)
+				stimulusCss : {color:'#31b404','font-size':'2em'}
+			},	
+			practiceCategory2 : 
+			{
+				name : 'Birds', 
+				title : {
+					media : {word : 'Birds'}, 
+					css : {color:'#31b404','font-size':'1.8em'}, 
+					height : 4,
+					startStimulus : {
+						media : {word : 'Pigeons, Swans, Crows, Ravens'}, 
+						css : {color:'#31b404','font-size':'1em'}, 
+						height : 2
+					}
+				}, 
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word : 'Pigeons'}, 
+					{word : 'Swans'}, 
+					{word : 'Crows'}, 
+					{word : 'Ravens'}
+				], 
+				//Stimulus css
+				stimulusCss : {color:'#31b404','font-size':'2em'}
+			},
+			categories : [  //As many categories you need.
+				{
+					name : 'category1', //Will appear in the data.
+					title : {
+						media : {word : 'Category1'}, //Name of the category presented in the task.
+						css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
+						height : 4, //Height (because we need to know where to put the next item in the title)
+						startStimulus : { 
+						//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
+							media : {word : 'cat1A, cat1B'}, 
+							css : {color:'#31b404','font-size':'1em'}, 
+							height : 2
+						}
+					}, 
+					stimulusMedia : [ //Stimuli content as PIP's media objects
+						{word : 'cat1A'}, 
+						{word : 'cat1B'}
+					], 
+					//Stimulus css (style of the stimuli)
+					stimulusCss : {color:'#31b404','font-size':'2em'}
+				},	
+				{
+					name : 'category2', 
+					title : {
+						media : {word : 'Category2'}, 
+						css : {color:'#31b404','font-size':'1.8em'}, 
+						height : 4,
+						startStimulus : {
+							media : {word : 'cat2A, cat2B'}, 
+							css : {color:'#31b404','font-size':'1em'}, 
+							height : 2
+						}
+					}, 
+					stimulusMedia : [ //Stimuli content as PIP's media objects
+						{word : 'cat2A'}, 
+						{word : 'cat2B'}
+					], 
+					//Stimulus css
+					stimulusCss : {color:'#31b404','font-size':'2em'}
+				}
+			]
+	});
 });
+
+
+
 
