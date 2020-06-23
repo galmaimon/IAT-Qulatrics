82d75dd0db2b388aba68c10a09ecc8d0011696e4
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-06-23 20:57:19 +0300

    Update exampleBIAT.js

--- exampleBIAT.js
+++ exampleBIAT.js
@@ -48,46 +48,55 @@ define(['pipAPI', 'https://galmaimon.github.io/BIAT.js'], function(APIConstructo
 				//Stimulus css
 				stimulusCss : {color:'#31b404','font-size':'2em'}
 			},
-			categories : [  //As many categories you need.
+				categories : [  //As many categories you need.
 				{
-					name : 'category1', //Will appear in the data.
+					name : 'Black people', //Will appear in the data.
 					title : {
-						media : {word : 'Category1'}, //Name of the category presented in the task.
+						media : {word : 'Black people'}, //Name of the category presented in the task.
 						css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
 						height : 4, //Height (because we need to know where to put the next item in the title)
 						startStimulus : { 
 						//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
-							media : {word : 'cat1A, cat1B'}, 
+							media : {word : 'Tyron, Malik, Terrell, Jazmin, Tiara, Shanice'}, 
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 2
 						}
 					}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
-						{word : 'cat1A'}, 
-						{word : 'cat1B'}
+                    {word: 'Tyron'},
+					{word: 'Malik'},
+					{word: 'Terrell'},
+					{word: 'Jazmin'},
+					{word: 'Tiara'},
+					{word: 'Shanice'}
 					], 
 					//Stimulus css (style of the stimuli)
 					stimulusCss : {color:'#31b404','font-size':'2em'}
 				},	
 				{
-					name : 'category2', 
+					name : 'White people', 
 					title : {
-						media : {word : 'Category2'}, 
+						media : {word : 'White people'}, 
 						css : {color:'#31b404','font-size':'1.8em'}, 
 						height : 4,
 						startStimulus : {
-							media : {word : 'cat2A, cat2B'}, 
+							media : {word : 'Jake, Connor, Bradley, Alison,Emma, Emily'}, 
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 2
 						}
 					}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
-						{word : 'cat2A'}, 
-						{word : 'cat2B'}
+                    {word: 'Jake'},
+					{word: 'Connor'},
+					{word: 'Bradley'},
+					{word: 'Allison'},
+					{word: 'Emma'},
+					{word: 'Emily'}
 					], 
 					//Stimulus css
 					stimulusCss : {color:'#31b404','font-size':'2em'}
 				}
+			
 			],
 			base_url : {//Where are your images at?
 				image : 'https://baranan.github.io/minno-tasks/images/'
