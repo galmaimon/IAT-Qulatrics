43192251f03ac7c7b482c77ba865ce0b554099b8
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-06-26 10:27:37 +0300

    Update BIAT.js

--- BIAT.js
+++ BIAT.js
@@ -739,11 +739,11 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor,Scorer, _) {
 				layout = layout.concat([				
 					{location:{top:1 + (params.focalCatTitle.height | 3)},
 						media:params.focalCatTitle.startStimulus.media, css:params.focalCatTitle.startStimulus.css}, 
-					{location:{top:1 + (params.focalCatTitle.height ) + (params.focalCatTitle.startStimulus.height )},
+					{location:{top:1 + (params.focalCatTitle.height | 10) + (params.focalCatTitle.startStimulus.height | 10)},
 						media:{word:'and'}, css:{color:'#000000','font-size':'1.8em'}}, 
-					{location:{top:8 + 1 + (params.focalCatTitle.height | 3) + (params.focalCatTitle.startStimulus.height | 3)}, 
+					{location:{top:10 + 1 + (params.focalCatTitle.height | 3) + (params.focalCatTitle.startStimulus.height | 3)}, 
 					media : params.focalAttTitle.media, css: params.focalAttTitle.css},
-					{location:{top:8 + 1 + (params.focalCatTitle.height | 3) + (params.focalCatTitle.startStimulus.height | 3) + (params.focalAttTitle.height | 3)}, 
+					{location:{top:10 + 1 + (params.focalCatTitle.height | 3) + (params.focalCatTitle.startStimulus.height | 3) + (params.focalAttTitle.height | 3)}, 
 					media : params.focalAttTitle.startStimulus.media, css: params.focalAttTitle.startStimulus.css}
 				]); 
 			}
