0a49c394df52a03c3d2be5da6248a6b52788b091
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-06-26 10:38:53 +0300

    Update BIAT.js

--- BIAT.js
+++ BIAT.js
@@ -743,7 +743,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor,Scorer, _) {
 						media:{word:'and'}, css:{color:'#000000','font-size':'1.8em'}}, 
 					{location:{top:10 + 1 + (params.focalCatTitle.height | 5) + (params.focalCatTitle.startStimulus.height | 5)}, 
 					media : params.focalAttTitle.media, css: params.focalAttTitle.css},
-					{location:{top:5 + 1 + (params.focalCatTitle.height | 3) + (params.focalCatTitle.startStimulus.height | 3) + (params.focalAttTitle.height | 3)}, 
+					{location:{top:10 + 1 + (params.focalCatTitle.height | 3) + (params.focalCatTitle.startStimulus.height | 3) + (params.focalAttTitle.height | 3)}, 
 					media : params.focalAttTitle.startStimulus.media, css: params.focalAttTitle.startStimulus.css}
 				]); 
 			}
