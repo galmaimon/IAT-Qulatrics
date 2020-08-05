83748371ca7952122388fd889d1a84aae71074c6
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-08-05 12:41:11 +0300

    Update IATFullScreen.js

--- IATFullScreen.js
+++ IATFullScreen.js
@@ -29,7 +29,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			isTouch:false, //Set whether the task is on a touch device.
 			//Set the canvas of the task
 			canvas : {
-				maxWidth: 725,
+				maxWidth: 2000,
 				proportions : 0.9,
 				background: '#ffffff',
 				borderWidth: 5,
