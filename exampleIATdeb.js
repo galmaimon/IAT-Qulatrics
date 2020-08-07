5708c8ec25db9ba6b394b4b316490b874583bbb0
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-08-07 18:49:24 +0300

    Update exampleIATdeb.js

--- exampleIATdeb.js
+++ exampleIATdeb.js
@@ -1,87 +1,102 @@
 define(['pipAPI', 'https://galmaimon.github.io/iatdeb.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
-
+var posWords = API.shuffle([
+            'Love', 'Cheer', 'Friend', 'Pleasure', 
+            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
+            'Smiling','Cherish', 'Excellent', 'Glad', 
+            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
+            'Excitement', 'Laughing', 'Attractive','Delightful', 
+            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
+            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
+            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph']);
+  var negWords = API.shuffle([
+            'Abuse', 'Grief', 'Poison', 'Sadness', 
+            'Pain', 'Despise', 'Failure', 'Nasty', 
+            'Angry', 'Detest', 'Horrible', 'Negative', 
+            'Ugly', 'Dirty', 'Gross', 'Evil', 
+            'Rotten','Annoy', 'Disaster', 'Horrific',  
+            'Scorn', 'Awful', 'Disgust', 'Hate', 
+            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
+            'Hatred', 'Hurtful', 'Sickening', 'Yucky']);
 	return iatExtension({
-
 		category1 : {
-			name : 'Native American', //Will appear in the data.
+			name : 'Old people', //Will appear in the data.
 			title : {
-				media : {word : 'Native American'}, //Name of the category presented in the task.
+				media : {word : 'Old people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-		{image: 'native1.jpg'},
-				{image: 'native2.jpg'},
-				{image: 'native3.jpg'},
-				{image: 'native4.jpg'},
-				{image: 'native5.jpg'},
-				{image: 'native6.jpg'},
-				{image: 'native7.jpg'},
-				{image: 'native8.jpg'}
-
+    		    {image: 'om1.jpg'},
+                {image: 'om2.jpg'},
+                {image: 'om3.jpg'},
+                {image: 'of1.jpg'},
+                {image: 'of2.jpg'},                 
+                {image: 'of3.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'White American', //Will appear in the data.
+			name : 'Young people', //Will appear in the data.
 			title : {
-				media : {word : 'White American'}, //Name of the category presented in the task.
+				media : {word : 'Young people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-	{image: 'euro1.jpg'},
-				{image: 'euro2.jpg'},
-				{image: 'euro3.jpg'},
-				{image: 'euro4.jpg'},
-				{image: 'euro5.jpg'},
-				{image: 'euro6.jpg'},
-				{image: 'euro7.jpg'},
-				{image: 'euro8.jpg'}
-			
+    		    {image: 'ym1.jpg'},
+                {image: 'ym2.jpg'},
+                {image: 'ym3.jpg'},
+                {image: 'yf1.jpg'},
+                {image: 'yf2.jpg'},
+                {image: 'yf3.jpg'}
 			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},
-		attribute1 :
+		},	
+	attribute1 :
 		{
-			name : 'Foreign',
+			name : 'Bad',
 			title : {
-				media : {word : 'Foreign'},
+				media : {word : 'Bad'},
 				css : {color:'#0000FF','font-size':'1.8em'},
 				height : 4 //Used to position the "Or" in the combined block.
 			},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-            	{image: 'world2.jpg'},
-				{image: 'world3.jpg'},
-				{image: 'world5.jpg'},
-				{image: 'world6.jpg'},
-				{image: 'world7.jpg'}
-				
+				{word: negWords[0]},
+				{word: negWords[1]},
+				{word: negWords[2]},
+				{word: negWords[3]},
+				{word: negWords[4]},
+				{word: negWords[5]},
+				{word: negWords[6]},
+				{word: negWords[7]}
 			],
 			//Stimulus css
 			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 		},
 		attribute2 :
 		{
-			name : 'American',
+			name : 'Good',
 			title : {
-				media : {word : 'American'},
+				media : {word : 'Good'},
 				css : {color:'#0000FF','font-size':'1.8em'},
 				height : 4 //Used to position the "Or" in the combined block.
 			},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-           	{image: 'us1.jpg'},
-				{image: 'us2.jpg'},
-				{image: 'us3.jpg'},
-				{image: 'us4.jpg'},
-				{image: 'us5.jpg'}			],
+				{word: posWords[0]},
+				{word: posWords[1]},
+				{word: posWords[2]},
+				{word: posWords[3]},
+				{word: posWords[4]},
+				{word: posWords[5]},
+				{word: posWords[6]},
+				{word: posWords[7]}
+			],
 			//Stimulus css
 			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 		},
-
 		base_url : {//Where are your images at?
 			image : 'https://baranan.github.io/minno-tasks/images/'
 		} 
