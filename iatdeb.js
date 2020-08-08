d486846be4c8b43b254e0ece76b0d27fc73d317b
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-08-08 17:35:00 +0300

    Update iatdeb.js

--- iatdeb.js
+++ iatdeb.js
@@ -1266,7 +1266,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			stimuli : [
 				{
 					inherit : 'Default',
-					media:{html:'<div class="result" style="background-color:lightyellow"><b><%=showFeedback()%></b></div>'},
+                    media : {word : piCurrent.debriefing}
                     
 				}
 			]
