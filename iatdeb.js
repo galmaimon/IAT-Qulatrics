fca341b5992d0406fc9e6862eec052215d8092fb
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-08-08 15:11:40 +0300

    Update iatdeb.js

--- iatdeb.js
+++ iatdeb.js
@@ -1263,8 +1263,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			stimuli : [
 				{
                     inherit : 'Default',
-					media : {word : (isTouch ? piCurrent.finalTouchText : piCurrent.debriefing)},
-				//	media : {word : (isTouch ? piCurrent.finalTouchText : piCurrent.feedback)}
+					media : {word : (isTouch ? piCurrent.finalTouchText : piCurrent.feedback)},
 				}
 			]
         });
