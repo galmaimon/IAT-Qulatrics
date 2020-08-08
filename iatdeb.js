fb12363e993939c901bda5affb2aff720453c6cd
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-08-08 13:50:10 +0300

    Update iatdeb.js

--- iatdeb.js
+++ iatdeb.js
@@ -351,7 +351,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
             // we save as CSV because qualtrics limits to 20K characters and this is more efficient.
             serialize: function (name, logs) {
                 var headers = ['block', 'trial', 'cond', 'comp', 'type', 'cat',  'stim', 'resp', 'err', 'rt', 'd', 'fb', 'bOrd'];
-                console.log(logs);
+                //console.log(logs);
                 var myLogs = [];
                 var iLog;
                 for (iLog = 0; iLog < logs.length; iLog++)
@@ -1324,7 +1324,8 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			scoreMessageObject.notEnough = piCurrent.notEnough;
 		}
 		//Set messages to the scorer.
-		scorer.addSettings('message',scoreMessageObject);
+        scorer.addSettings('message',scoreMessageObject);
+        console.log("FINISHED SHOULD RETURN SCORE")
         console.log(scorer.message);
 
 		return API.script;
