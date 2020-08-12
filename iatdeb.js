b657c8ee4ee7a9c79292338eb0607063afb1b303
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-08-12 10:06:54 +0300

    Update iatdeb.js

--- iatdeb.js
+++ iatdeb.js
@@ -1294,7 +1294,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 
         //debrefing
         trialSequence.push({
-            stimuli: [{data:{handle:'feedbackstim'},media :{word: '<%=piCurrent.feedback%>'}}],
+            stimuli: [{data:{handle:'feedbackstim'},media :{word: '<%=current.feedback%>'}}],
                 //input: [{handle:'space',on:'space'}],
                 layout: [{media :{word: 'nn'}}],
                 interactions: [{
