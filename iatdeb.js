9e9ef1af615dccd9eb1f4f28b84818f31ae82927
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-08-19 13:35:54 +0300

    Update iatdeb.js

--- iatdeb.js
+++ iatdeb.js
@@ -25,22 +25,13 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			 /*showDebriefing is true witch mean that the user will see his feeadback at the end of the test.
 		If you don't want the feedback to be shown to the user change this value to be false, the score of the test will be save at both cases
 		*/
-		showDebriefing:true,
+			showDebriefing:true,
 			/* 
 		fullscreen mode is false, if full-screen is wanted change fullscreen value to be true,
         changing fullscreen value to be true will make the task fullscreen after the first question in Qualtrics, which mean that the trials will begin in full screen
 		*/
-		//fullscreen:false,
-        // if(fullscreen){
-        //     var el = document.documentElement;
-		//     var rfs = el.requestFullscreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
-		//     if (rfs) rfs.call(el);
-		//     else if(window.ActiveXObject){
-        // // for Internet Explorer
-    	//     var wscript = new window.ActiveXObject('WScript.Shell');
-    	//     if (wscript!=null) wscript.SendKeys('{F11}');
-        //     }
-        // },
+			fullscreen:true,
+        
 			isTouch:false, //Set whether the task is on a touch device.
 			//Set the canvas of the task
 			canvas : {
@@ -466,6 +457,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 		// are we on the touch version
 		var isTouch = piCurrent.isTouch;
 		var showDebriefing=piCurrent.showDebriefing;
+		var fullscreen=piCurrent.fullscreen;
 		//We use these objects a lot, so let's read them here
 		var att1 = piCurrent.attribute1;
 		var att2 = piCurrent.attribute2;
@@ -483,6 +475,16 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			att2.stimulusCss.maxHeight = maxH;
 			cat1.stimulusCss.maxHeight = maxH;
 			cat2.stimulusCss.maxHeight = maxH;
+		}
+		if(fullscreen){
+			var el = document.documentElement;
+			var rfs = el.requestFullscreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
+			if (rfs) rfs.call(el);
+			else if(window.ActiveXObject){
+		// for Internet Explorer
+			var wscript = new window.ActiveXObject('WScript.Shell');
+			if (wscript!=null) wscript.SendKeys('{F11}');
+			}
 		}
 
 		//Set the attribute on the left.
