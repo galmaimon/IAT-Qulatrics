c6c7a56ab49f1eb5589bb8fbc19d50a9737e34d9
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-08-19 13:57:19 +0300

    Update exampleIATdeb.js

--- exampleIATdeb.js
+++ exampleIATdeb.js
@@ -1,8 +1,18 @@
-define(['pipAPI', 'https://galmaimon.github.io/iatdeb.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://galmaimon.github.io/iatfull.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
 		
+ /*showDebriefing is true witch mean that the user will see his feeadback at the end of the test.
+		If you don't want the feedback to be shown to the user change this value to be false, the score of the test will be save at both cases
+		*/
+		showDebriefing:true,
+		/* 
+	fullscreen mode is false, if full-screen is wanted change fullscreen value to be true,
+	changing fullscreen value to be true will make the task fullscreen after the first question in Qualtrics, which mean that the trials will begin in full screen
+	*/
+		fullscreen:true,
+	
 		category1 : {
 			name : 'Career', //Will appear in the data.
 			title : {
