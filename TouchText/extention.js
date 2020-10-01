03984cb3708394a7c830c9ee9cd771fa2cf82d67
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-10-01 18:49:25 +0300

    Update extention.js

--- TouchText/extention.js
+++ TouchText/extention.js
@@ -25,7 +25,7 @@ define(['pipAPI','https://galmaimon.github.io/qiattouch.js'], function(APIConstr
   
     return iatExtension({
         isTouch:true,
-	showDebriefing:true,
+	showDebriefing:false,
         
         category1 : {
             name : 'Black people', //Will appear in the data.
