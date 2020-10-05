a25a6af67e44dda1eb438cd6dc2a1e2c69666eeb
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-10-05 09:59:00 +0300

    Update extention.js

--- TouchText/extention.js
+++ TouchText/extention.js
@@ -24,7 +24,7 @@ define(['pipAPI','https://galmaimon.github.io/qiattouch.js'], function(APIConstr
             'Hatred', 'Hurtful', 'Sickening', 'Yucky']);
   
     return iatExtension({
-        isTouch:false,
+        isTouch:true,
 	showDebriefing:true,
         
         category1 : {
